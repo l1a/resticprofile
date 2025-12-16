89754c02a63e502b772b4469960a235c35d8c1c1
Ken Tobias <634380+l1a@users.noreply.github.com>
2025-12-16 11:45:11 -0800

    fix(priority): check current priority before setting to avoid permission denied
    Also added debug logging with context

--- priority/unix.go
+++ priority/unix.go
@@ -3,6 +3,7 @@
 package priority
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/creativeprojects/clog"
@@ -11,7 +12,7 @@ import (
 
 const (
 	selfPID      = 0
-	errorMessage = "cannot set process group priority, restic will run with the default priority: %w"
+	errorMessage = "cannot set process group priority to %d, restic will run with the default priority: %w"
 )
 
 // SetNice sets the unix "nice" value of the current process
@@ -22,14 +23,26 @@ func SetNice(priority int) error {
 		return fmt.Errorf("unexpected priority value %d", priority)
 	}
 
+	currentPriority, err := unix.Getpriority(unix.PRIO_PGRP, selfPID)
+	if err == nil {
+		clog.Debugf("current process group priority is %d", 20-currentPriority)
+		if 20-currentPriority >= priority {
+			// If the process is already running at a lower priority (higher nice value)
+			// than requested, we don't need to change it. This avoids permission errors
+			// when running as a normal user who cannot increase priority (lower nice value).
+			clog.Debugf("current priority %d is already lower or equal to requested %d, skipping", 20-currentPriority, priority)
+			return nil
+		}
+	}
+
 	clog.Debugf("setting process group priority to %d", priority)
 	err = unix.Setpriority(unix.PRIO_PGRP, selfPID, priority)
 	if err != nil {
-		if err.Error() == "operation not permitted" {
+		if errors.Is(err, unix.EPERM) || errors.Is(err, unix.EACCES) {
 			// try again after creating a new process group
 			return setNewProcessGroup(priority)
 		}
-		return fmt.Errorf(errorMessage, err)
+		return fmt.Errorf(errorMessage, priority, err)
 	}
 
 	return nil
@@ -38,11 +51,11 @@ func SetNice(priority int) error {
 func setNewProcessGroup(priority int) error {
 	err := unix.Setpgid(selfPID, 0)
 	if err != nil {
-		return fmt.Errorf(errorMessage, err)
+		return fmt.Errorf("cannot set new process group (priority %d): %w", priority, err)
 	}
 	err = unix.Setpriority(unix.PRIO_PGRP, selfPID, priority)
 	if err != nil {
-		return fmt.Errorf(errorMessage, err)
+		return fmt.Errorf("cannot set priority %d on new process group: %w", priority, err)
 	}
 	return nil
 }
